f605fedf966192fa51e3ba869a9baf8ec3556e6d
lvallabhaneni <lvallabhaneni@griddynamics.com>
2026-05-14 15:31:35 +0530

    remove unwanted changes

--- mobile/index.js
+++
@@ -1,6 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from './App';
-
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App)
-// and ensures the correct environment is set up for Expo Go and native builds.
-registerRootComponent(App);
